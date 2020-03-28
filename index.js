850b30fa1209aa0ac365edf82cba974d76f91880
Jeffrey Vaz <jeff.j.vaz@gmail.com>
2020-03-28 10:19:40 -0400

    add contact with form submission data

--- index.js
+++ index.js
@@ -16,8 +16,7 @@ app.get('/', (req, res) => {
   res.render('landing');
 });
 
-
-function formv3(){
+const formv3 = (email, firstname, lastname) => {
   // Create the new request 
   var xhr = new XMLHttpRequest();
   var url = 'https://api.hsforms.com/submissions/v3/integration/submit/7388454/6153d0b7-b2fa-4297-86fa-8aab202b232f'
@@ -28,15 +27,18 @@ function formv3(){
     "fields": [
       {
         "name": "email",
-        "value": "example@example.com"
+        // "value": "jac@gmail.com"
+        "value": email
       },
       {
         "name": "firstname",
-        "value": "Jeff"
+        // "value": "Jac"
+        "value": firstname
       },
       {
         "name": "lastname",
-        "value": "Vaz"
+        // "value": "Vaz"
+        "value": lastname
       }
     ],
     // "context": {
@@ -65,17 +67,17 @@ function formv3(){
   // Sets the value of the 'Content-Type' HTTP request headers to 'application/json'
   xhr.setRequestHeader('Content-Type', 'application/json');
 
-  // xhr.onreadystatechange = function() {
-  //     if(xhr.readyState == 4 && xhr.status == 200) { 
-  //         alert(xhr.responseText); // Returns a 200 response if the submission is successful.
-  //     } else if (xhr.readyState == 4 && xhr.status == 400){ 
-  //         alert(xhr.responseText); // Returns a 400 error the submission is rejected.          
-  //     } else if (xhr.readyState == 4 && xhr.status == 403){ 
-  //         alert(xhr.responseText); // Returns a 403 error if the portal isn't allowed to post submissions.           
-  //     } else if (xhr.readyState == 4 && xhr.status == 404){ 
-  //         alert(xhr.responseText); //Returns a 404 error if the formGuid isn't found     
-  //     }
-  //    }
+  xhr.onreadystatechange = function() {
+    if(xhr.readyState == 4 && xhr.status == 200) { 
+        console.log(xhr.responseText); // Returns a 200 response if the submission is successful.
+    } else if (xhr.readyState == 4 && xhr.status == 400){ 
+        console.log(xhr.responseText); // Returns a 400 error the submission is rejected.          
+    } else if (xhr.readyState == 4 && xhr.status == 403){ 
+        console.log(xhr.responseText); // Returns a 403 error if the portal isn't allowed to post submissions.           
+    } else if (xhr.readyState == 4 && xhr.status == 404){ 
+        console.log(xhr.responseText); //Returns a 404 error if the formGuid isn't found     
+    }
+   }
 
 
   // Sends the request 
@@ -85,69 +87,7 @@ function formv3(){
 }
 
 app.post('/', (req, res) => {
-    // Create the new request 
-    var xhr = new XMLHttpRequest();
-    var url = 'https://api.hsforms.com/submissions/v3/integration/submit/7388454/6153d0b7-b2fa-4297-86fa-8aab202b232f'
-    
-    // Example request JSON:
-    var data = {
-      // "submittedAt": "1517927174000",
-      "fields": [
-        {
-          "name": "email",
-          "value": "jeffrey@gmail.com"
-        },
-        {
-          "name": "firstname",
-          "value": "Jeff"
-        },
-        {
-          "name": "lastname",
-          "value": "Vaz"
-        }
-      ],
-      // "context": {
-      //   "hutk": ':hutk', // include this parameter and set it to the hubspotutk cookie value to enable cookie tracking on your submission
-      //   "pageUri": "www.example.com/page",
-      //   "pageName": "Example page"
-      // },
-      "legalConsentOptions":{ // Include this object when GDPR options are enabled
-        "consent":{
-          "consentToProcess":true,
-          "text":"I agree to allow Example Company to store and process my personal data.",
-          "communications":[
-            {
-              "value":true,
-              "subscriptionTypeId":999,
-              "text":"I agree to receive marketing communications from Example Company."
-            }
-          ]
-        }
-      }
-    }
-  
-    var final_data = JSON.stringify(data)
-  
-    xhr.open('POST', url);
-    // Sets the value of the 'Content-Type' HTTP request headers to 'application/json'
-    xhr.setRequestHeader('Content-Type', 'application/json');
-  
-    xhr.onreadystatechange = function() {
-        if(xhr.readyState == 4 && xhr.status == 200) { 
-            console.log(xhr.responseText); // Returns a 200 response if the submission is successful.
-        } else if (xhr.readyState == 4 && xhr.status == 400){ 
-            console.log(xhr.responseText); // Returns a 400 error the submission is rejected.          
-        } else if (xhr.readyState == 4 && xhr.status == 403){ 
-            console.log(xhr.responseText); // Returns a 403 error if the portal isn't allowed to post submissions.           
-        } else if (xhr.readyState == 4 && xhr.status == 404){ 
-            console.log(xhr.responseText); //Returns a 404 error if the formGuid isn't found     
-        }
-       }
-  
-  
-    // Sends the request 
-    
-    xhr.send(final_data)
+  formv3(req.body.email, req.body.firstname, req.body.lastname);
   res.send('thanks for submitting');
 })
 
