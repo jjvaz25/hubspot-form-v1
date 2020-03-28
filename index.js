17c43da6b65c36675a53bddeba612dcc0d23f974
Jeffrey Vaz <jeff.j.vaz@gmail.com>
2020-03-28 10:59:11 -0400

    add port for app.listen

--- index.js
+++ index.js
@@ -92,4 +92,4 @@ app.post('/', (req, res) => {
 })
 
 
-app.listen(3000, () => console.log('listening on port 3000'));
+app.listen(process.env.PORT || 3000, () => console.log('listening on port 3000'));
